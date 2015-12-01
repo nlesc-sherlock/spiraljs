5e8661db24e1abf791083a2e8cd11e6caf2c12c9
mkuzak <mateusz.kuzak@gmail.com>
2015-12-01 18:36:34 +0100

    Add beautify task

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,39 @@
 var gulp = require("gulp");
 var tslint = require("gulp-tslint");
 var eslint = require("gulp-eslint");
+var beautify = require("gulp-beautify");
 var ts = require("gulp-typescript");
-
 
 // tasks
 // lint
-gulp.task('eslint', function(){
+gulp.task('eslint', function() {
     return gulp.src(['**/*.js', '!node_modules/**', '!bower_components/**'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 })
 
-gulp.task('tslint', function(){
-      return gulp.src('src/**/*.ts')
+gulp.task('tslint', function() {
+    return gulp.src('src/**/*.ts')
         .pipe(tslint())
         .pipe(tslint.report('verbose'));
 });
 
+gulp.task('beautify', function() {
+    gulp.src('*.js')
+        .pipe(beautify())
+        .pipe(gulp.dest('./'));
+    gulp.src('src/ts/*.ts')
+        .pipe(beautify())
+        .pipe(gulp.dest('src/ts'));
+});
+
+
 var tsProject = ts.createProject('tsconfig.json');
 // compile typescript
-gulp.task('ts', function () {
+gulp.task('ts', function() {
     var tsResult = tsProject.src() // instead of gulp.src(...) 
         .pipe(ts(tsProject));
 
     return tsResult.js.pipe(gulp.dest("./"));
 });
-
