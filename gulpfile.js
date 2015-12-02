840cf6f2954d8cea2fa46e9628e60bc46a5cfe75
mkuzak <mateusz.kuzak@gmail.com>
2015-12-02 11:53:51 +0100

    Browser-sync in gulp with watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,24 @@ var gulp = require("gulp");
 var tslint = require("gulp-tslint");
 var eslint = require("gulp-eslint");
 var beautify = require("gulp-beautify");
-var ts = require("gulp-typescript");
+var ts = require("gulp-typescript")
+var browsersync = require("browser-sync").create();
 
 // tasks
-// lint
+// lint javascript and typescript
 gulp.task('eslint', function() {
     return gulp.src(['**/*.js', '!node_modules/**', '!bower_components/**'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 })
-
 gulp.task('tslint', function() {
     return gulp.src('src/**/*.ts')
         .pipe(tslint())
         .pipe(tslint.report('verbose'));
 });
 
+// cleanup files
 gulp.task('beautify', function() {
     gulp.src('*.js')
         .pipe(beautify())
@@ -28,7 +29,7 @@ gulp.task('beautify', function() {
         .pipe(gulp.dest('src/ts'));
 });
 
-
+// compile javascript from typescript
 var tsProject = ts.createProject('tsconfig.json');
 // compile typescript
 gulp.task('ts', function() {
@@ -38,7 +39,19 @@ gulp.task('ts', function() {
     return tsResult.js.pipe(gulp.dest("./"));
 });
 
+// copy html and css files to build
 gulp.task('copy-build', function() {
     gulp.src('./src/*.html').pipe(gulp.dest('./build/'))
     gulp.src('./src/styles/*.css').pipe(gulp.dest('./build/styles/'))
+});
+
+// run BrowserSync
+gulp.task('browser-sync', function() {
+    browsersync.init({
+        server: {
+            baseDir: ["./", "build"],
+            index: "build/index.html"
+        },
+        files: ["build/**/*.js", "build/**/*.css", "build/**/*.html"]
+    });
 });
