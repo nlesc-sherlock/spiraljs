25b07f74e356a26c71ca36503458c85ccca3630e
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2015-12-03 07:48:34 +0100

    some more content for crossfilter class

--- src/ts/crossfilter.ts
+++ src/ts/crossfilter.ts
@@ -1,2 +1,18 @@
 // crossfilter placeholder
 // TODO to be filled in
+
+
+// TODO add 'extends'
+class CrossFilter  {
+
+    private _data: IDataRow[];
+
+    constructor () {
+        // constructor function
+    }
+
+
+
+
+}
+
