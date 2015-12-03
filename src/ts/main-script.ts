e451a1e6340a90aae3b56ffbbf8305141329fc2d
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2015-12-03 07:55:40 +0100

    added punchcard and spiral objects; shuffled some code around

--- src/ts/main-script.ts
+++ src/ts/main-script.ts
@@ -1,49 +1,59 @@
 
-let dataloader :DataLoader;
-let map        :Map;
-let histogram  :Histogram;
-let timeline   :Timeline;
-
 
 function doit(data: any) {
+
+    let map: Map;
+    let histogram: Histogram;
+    let timeline: Timeline;
+    let punchcard: Punchcard;
+    let spiral: Spiral;
+
 
     // make a new map
     map = new Map('leaflet');
     map.binddata(data);
     map.circleMarkerOptions = {
-        fillColor  : '#F80',
+        fillColor: '#F80',
         fillOpacity: 0.5,
-        stroke     : true,
-        color      : '#000'
+        stroke: true,
+        color: '#000'
     };
     map.circleMarkerRadius = 6;
     map.showCrimeLocations();
 
 
     // make the histogram and then add it to the timeline
-    histogram = new Histogram(dataloader.data);
-    console.log(histogram);
+    histogram = new Histogram(data);
 
-    let size:ISize = {
+    let size: ISize = {
         width: window.innerWidth * 0.9,
         height: window.innerHeight * 0.4
     };
-    let padding:IPadding = {top: 40,
+
+    let padding: IPadding = {
+        top: 40,
         right: 40,
         bottom: 40,
         left: 50
     };
     timeline = new Timeline(size, padding, histogram);
 
+    punchcard = new Punchcard();
+    punchcard.binddata(data);
+
+    spiral = new Spiral();
+    spiral.binddata(data);
+
 };
 
 
 
-// make a new dataloader
-dataloader = new DataLoader();
 
-// configure the dataLoader
-dataloader.limit = 50000;
+// make a new dataloader
+let dataloader: DataLoader = new DataLoader();
+
+// configure the dataloader
+dataloader.limit = 500;
 
 // set the offset to a large value to get to the more recent records (the
 // results are sorted by increasing date); the more recent records are more
@@ -52,5 +62,4 @@ dataloader.offset = 0;
 
 // load the data
 dataloader.loadData(doit);
-
 
