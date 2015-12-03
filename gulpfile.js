13906be20dd5e3e5158051cccb3476f2caa79210
mkuzak <mateusz.kuzak@gmail.com>
2015-12-03 08:14:38 +0100

    Add gulp help

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp = require("gulp");
+var gulp = require('gulp-help')(require('gulp'));
 var tslint = require("gulp-tslint");
 var eslint = require("gulp-eslint");
 var beautify = require("gulp-beautify");
@@ -10,25 +10,27 @@ var browsersync = require("browser-sync").create();
 
 // tasks
 // lint javascript and typescript
-gulp.task('eslint', function() {
+gulp.task('eslint', 'Lints javascript files with eslint', function() {
     return gulp.src(['**/*.js', '!node_modules/**', '!bower_components/**'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 })
-gulp.task('tslint', function() {
-    return gulp.src('src/**/*.ts')
-        .pipe(tslint())
-        .pipe(tslint.report('verbose'));
+gulp.task('tslint',
+    'Lints typescript with tslint according to tslint.json present',
+    function() {
+        return gulp.src('src/**/*.ts')
+            .pipe(tslint())
+            .pipe(tslint.report('verbose'));
 });
 
 // cleanup files
-gulp.task('beautify-js', function() {
+gulp.task('beautify-js', false, function() {
     gulp.src('*.js')
         .pipe(beautify())
         .pipe(gulp.dest('./'));
 });
-gulp.task('beautify-ts', function() {      
+gulp.task('beautify-ts', false, function() {      
     gulp.src('src/ts/*.ts')
         .pipe(beautify())
         .pipe(gulp.dest('src/ts'));
@@ -37,40 +39,48 @@ gulp.task('beautify-ts', function() {
 // compile javascript from typescript
 var tsProject = ts.createProject('tsconfig.json');
 // compile typescript
-gulp.task('ts', ['tslint'], function() {
-    var tsResult = tsProject.src()
-        .pipe(sourcemaps.init())
-        .pipe(ts(tsProject));
+gulp.task('ts',
+    'Compiles typescript to javascript according to tsconfig.json',
+    ['tslint'], function() {
+        var tsResult = tsProject.src()
+            .pipe(sourcemaps.init())
+            .pipe(ts(tsProject));
 
-    return tsResult.js
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest("./"));
+        return tsResult.js
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest("./"));
 });
 
 
 //concatenate css files
-gulp.task('concat-css', function () {
-  return gulp.src('src/**/*.css')
-    .pipe(concatCss('bundle.css'))
-    .pipe(gulp.dest('build/styles/'));
+gulp.task('concat-css',
+    'Concatenates css files',
+    function () {
+        return gulp.src('src/**/*.css')
+        .pipe(concatCss('bundle.css'))
+        .pipe(gulp.dest('build/styles/'));
 });
 
 // copy html and css files to build
-gulp.task('copy-build', function() {
+gulp.task('copy-build',
+    'Copies html to build directory',
+    function() {
     gulp.src('./src/*.html').pipe(gulp.dest('./build/'))
-    gulp.src('./src/styles/*.css').pipe(gulp.dest('./build/styles/'))
 });
 
 
 // watch and build on change
-gulp.task('watch', ['ts'], function(){
+gulp.task('watch', false, ['ts'], function(){
    gulp.watch('src/**/*.ts', ['ts']);
    gulp.watch('./*.js', ['eslint', 'beautify-js']);
-   gulp.watch(['src/*.html', './src/styles/*.css'], ['copy-build'])
+   gulp.watch(['src/*.html'], ['copy-build']);
+   gulp.watch(['./src/styles/*.css'], ['concat-css']);
 });
 
 // run BrowserSync
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync',
+    'Serves files with BrowserSync, starts and restarts browser on changes',
+    function() {
     browsersync.init({
         server: {
             baseDir: ['./', 'build'],
@@ -83,4 +93,6 @@ gulp.task('browser-sync', function() {
         .on('change', browsersync.reload);
 });
 
-gulp.task('dev-watch', ['ts', 'copy-build', 'watch', 'browser-sync']);
+gulp.task('dev-watch',
+    'Watches files for development',
+    ['ts', 'concat-css', 'copy-build', 'watch', 'browser-sync']);
