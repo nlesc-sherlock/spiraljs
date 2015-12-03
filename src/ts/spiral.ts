8ed516e5e6a2c0cecce66dc26496c69e8230244c
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2015-12-03 07:46:36 +0100

    some more content for spiral class

--- src/ts/spiral.ts
+++ src/ts/spiral.ts
@@ -1,2 +1,32 @@
 // placeholder for spiral
 // TODO to be filled in
+
+
+
+class Spiral {
+
+    private _data: IDataRow[];
+
+    constructor () {
+        // constructor function
+    }
+
+
+
+
+    public binddata(data:any) {
+        // function used to bind data to this object
+        this._data = data;
+
+    }
+
+
+
+
+    public get data():any {
+        return this._data;
+    }
+
+
+}
+
