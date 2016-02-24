2abe15bafbf065b7936df450ddaaecbe4a989fc0
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-02-24 17:56:20 +0100

    partial commit: dc based punchcard works...almost

--- src/ts/punchcard.ts
+++ src/ts/punchcard.ts
@@ -1,29 +1,111 @@
-// placeholder for punchcard
-// TODO to be filled in
+/// <reference path="../../typings/crossfilter/crossfilter.d.ts" />
+/// <reference path="../../typings/d3/d3.d.ts" />
+/// <reference path="../../typings/dcjs/dc.d.ts" />
+/// <reference path="../../typings/moment/moment.d.ts" />
+
 
 
 class Punchcard {
 
-    private _data: IDataRow[];
+    private _cf       : CrossFilter.CrossFilter<IDataRow>;
+    private _dim      : any;
+    private _domElem  : HTMLElement;
+    private _domElemId: string;
 
-    constructor () {
-        // constructor function
+
+
+
+    constructor (cf: any, domElemId: string) {
+
+        this.cf = cf;
+
+        this.domElemId = domElemId;
+
+        this.domElem = document.getElementById(this.domElemId);
+
+        // all the dimensions are collected into one object, dim, which is
+        // initialized here:
+        this.dim = {};
+
     }
 
 
 
 
-    public binddata(data:any) {
-        // function used to bind data to this object
-        this._data = data;
-
+    public defineDimensions() {
+        this.dim.date = this.cf.dimension(function(d:IDataRow){
+            return d.momentStartOfDay;
+        });
+        this.dim.timeOfDay = this.cf.dimension(function(d:IDataRow){
+            return d.timeOfDay;
+        });
     }
 
 
 
 
-    public get data():any {
-        return this._data;
+    public draw() {
+
+        let minDate: moment.Moment;
+        let maxDate: moment.Moment;
+
+        minDate = this.dim.date.bottom(1)[0].momentStartOfDay;
+        maxDate = this.dim.date.top(1)[0].momentStartOfDay;
+
+        let measure = this.dim.date.group().reduceCount();
+
+        dc.bubbleChart('#' + this.domElemId)
+            .width(990)
+            .height(500)
+            .margins({top: 10, right: 50, bottom: 30, left: 60})
+            .dimension(this.dim.date)
+            .group(measure)
+            .x(d3.time.scale().domain([minDate.toDate(), maxDate.toDate()]))
+            .y(d3.scale.linear().domain([0, 24]))
+            .r(d3.scale.linear().domain([0, 1000]))
+            .maxBubbleRelativeSize(0.1)
+            .elasticX(true)
+            .elasticY(true)
+            .elasticRadius(true)
+            .yAxisLabel('Total number of arrests')
+            .renderHorizontalGridLines(true)
+            .renderVerticalGridLines(true);
+    }
+
+
+
+
+
+    private set cf(cf:any) {
+        this._cf = cf;
+    }
+
+    private get cf():any {
+        return this._cf;
+    }
+
+    private set dim(dim:any) {
+        this._dim = dim;
+    }
+
+    private get dim():any {
+        return this._dim;
+    }
+
+    private set domElem(domElem:HTMLElement) {
+        this._domElem = domElem;
+    }
+
+    private get domElem():HTMLElement {
+        return this._domElem;
+    }
+
+    private set domElemId(domElemId:string) {
+        this._domElemId = domElemId;
+    }
+
+    private get domElemId():string {
+        return this._domElemId;
     }
 
 
