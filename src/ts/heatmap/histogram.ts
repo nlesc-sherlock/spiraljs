282b900c80106978d9fb8c2b63e93f0c366fca43
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-02-25 18:20:37 +0100

    changed type to number for easier crossfilter operations

--- src/ts/heatmap/histogram.ts
+++ src/ts/heatmap/histogram.ts
@@ -4,6 +4,7 @@ interface IDataRow {
     moment           : moment.Moment;
     momentStartOfDay : moment.Moment;
     casenumber       : string;
+    dayOfWeek        : number;
     description      : string;
     primary          : string;
     latitude         : number;
