72366fe7d244147c83e644cf5f1432ae89cc0146
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-03-29 18:33:15 +0200

    punchcards complete for now

--- src/ts/main-script.ts
+++ src/ts/main-script.ts
@@ -58,12 +58,12 @@ function doit(data: IDataRow[]) {
 let dataloader: DataLoader = new DataLoader();
 
 // configure the dataloader
-dataloader.limit = 10000;
+dataloader.limit = 5000;
 
 // set the offset to a large value to get to the more recent records (the
 // results are sorted by increasing date); the more recent records are more
 // likely to have valid coordinates.
-dataloader.offset = 1260000;
+dataloader.offset = 5560000;
 
 // load the data
 dataloader.loadData(doit);
