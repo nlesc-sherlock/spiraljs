2d365485a6183bfd76cac7330339513f3e2f50e4
Johan Hidding <j.hidding@esciencecenter.nl>
2016-05-10 14:45:39 +0200

    commented dsp.ts

--- src/ts/spiralchart/dsp.ts
+++ src/ts/spiralchart/dsp.ts
@@ -1,3 +1,14 @@
+/**
+ * DSP module.
+ *
+ * Contains functionality to do digital signal processing on
+ * the time series that we try to visualise. In particular we experiment
+ * using the Fourier domain to guide the user to possible areas of interest.
+ */
+
+/**
+ * Complex number arithmetic
+ */
 module Complex {
     /**
      * Complex number class
@@ -5,28 +16,39 @@ module Complex {
     export class Complex {
         constructor (public real: number, public imag: number) {}
 
-        // the complex conjugate: (a + ib) -> (a - ib)
+        /**
+         * @returns the complex conjugate: (a + ib) -> (a - ib)
+         */
         get conjugate(): Complex {
             return new Complex(this.real, -this.imag);
         }
 
-        // the square function: (a + ib) -> a^2 - b^2
+        /**
+         * @returns the square function: (a + ib) -> a^2 - b^2
+         */
         public square(): number {
             return this.real * this.real - this.imag * this.imag;
         }
 
-        // the length square: (a + ib) -> a^2 + b^2
+        /**
+         * @returns the length square: (a + ib) -> a^2 + b^2
+         */
         public norm2(): number {
             return Math.sqrt(this.real * this.real + this.imag * this.imag);
         }
 
-        // inverse number: x -> y such that x*y == 1
+        /**
+         * @returns inverse number: x -> y such that x y == 1
+         */
         public inverse(): Complex {
             let s: number = this.norm2();
             return new Complex(this.real / s, -this.imag / s);
         }
 
-        // complex multiplication: (a + ib) * (c + id) -> (a*c - b*d + i(a*d + b*c))
+        /**
+         * @param a other complex number.
+         * @returns complex multiple: (a + ib) * (c + id) -> (a c - b d + i(a d + b c))
+         */
         public times(a: Complex): Complex {
             return new Complex(
                 this.real * a.real - this.imag * a.imag,
@@ -34,19 +56,25 @@ module Complex {
             );
         }
 
-        // multiply by real number
+        /**
+         * @returns multiple by real number: (a + ib) c -> (a c + i b c)
+         */
         public times_real(a: number): Complex {
             return new Complex(this.real * a, this.imag * a);
         }
 
-        // complex addition: (a + ib) + (c + id) -> (a+c + i(b+d))
+        /**
+         * @returns complex sum: (a + ib) + (c + id) -> (a+c + i(b+d))
+         */
         public plus(a: Complex): Complex {
             return new Complex(
                 this.real + a.real, this.imag + a.imag
             );
         }
 
-        // complex subtraction
+        /**
+         * @returns complex difference: (a + ib) - (c + id) -> (a-c + i(b-d))
+         */
         public minus(a: Complex): Complex {
             return new Complex(
                 this.real - a.real, this.imag - a.imag
@@ -54,19 +82,44 @@ module Complex {
         }
     }
 
+    /**
+     * @param a complex number
+     * @returns exp(a + ib) -> exp(a) (cos(b) + i sin(b))
+     */
     export function exp(a: Complex): Complex {
         let s: number = Math.exp(a.real);
         return new Complex(s * Math.cos(a.imag), s * Math.sin(a.imag));
     }
 
+    /**
+     * @param a real number
+     * @returns exp(ia)
+     */
     export function expi(a: number): Complex {
         return new Complex(Math.cos(a), Math.sin(a));
     }
 }
 
+/**
+ * The RFFT or Rather Fast Fourier Transform.
+ *
+ * This module has the forward as well as inverse transform. See
+ * https://en.wikipedia.org/wiki/Discrete_Fourier_transform for more information
+ * on the discrete Fourier transform.
+ * To answer the question on everyone's mind: the inverse transform is normalised
+ * such that it returns the original series (barring round-off errors).
+ */
 module FFT {
     import complex = Complex.Complex;
 
+    /**
+     * Forward Fourier transform (1d). This uses the Cooley–Tukey algorithm
+     * in a recursive fashion. This is not an optimal implementation, but it
+     * is fast enough for our purposes.
+     *
+     * @param s list of complex numbers.
+     * @returns discrete Fourier sum of the series.
+     */
     export function fft(s: complex[]): complex[] {
         let N: number = s.length;
 
@@ -96,6 +149,12 @@ module FFT {
         return y;
     }
 
+    /**
+     * Inverse Fourier transform
+     *
+     * @param s list of complex numbers
+     * @returns the inverse discrete Fourier sum of the series.
+     */
     export function ifft(s: complex[]): complex[] {
         let y = fft(s.map(z => z.conjugate));
         return y.map(z => z.conjugate.times_real(1.0 / s.length));
