ada0b3d27a87b944eacc4fcf9d3076114d95a221
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-12-05 16:52:22 +0100

    minor corrections

--- src/LineSpiral.ts
+++ src/LineSpiral.ts
@@ -1,6 +1,6 @@
 import * as d3 from 'd3';
 
-import { SpiralBase }      from './SpiralBase'; 
+import { SpiralBase }      from './SpiralBase';
 import { HistogramOutput } from './basechart';
 import { Polar }           from './basechart';
 
