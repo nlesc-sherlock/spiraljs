1efae676191a7b4621505c8acd33e8f6bbb5b725
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-12-05 15:28:49 +0100

    moved part of src/dsp.ts to its own file

---
+++ src/FFT.ts
@@ -0,0 +1,36 @@
+import complex = Complex.Complex;
+
+export function fft(s: complex[]): complex[] {
+    let N: number = s.length;
+
+    if (N === 1) { return [s[0]]; }
+
+    if (N % 2 !== 0) { throw new Error('FFT: Size of array must be power of 2.'); }
+
+    let r = new Array<complex>(N / 2);
+    for (var j = 0; j < N / 2; ++j) {
+        r[j] = s[j * 2];
+    }
+    let p = fft(r);
+
+    for (var j = 0; j < N / 2; ++j) {
+        r[j] = s[j * 2 + 1];
+    }
+    let q = fft(r);
+
+    var y = new Array<complex>(N);
+    for (var k = 0; k < N / 2; ++k) {
+        let wk = Complex.expi(-2 * k * Math.PI / N);
+        let qk = wk.times(q[k]);
+        y[k]         = p[k].plus(qk);
+        y[k + N / 2] = p[k].minus(qk);
+    }
+
+    return y;
+}
+
+export function ifft(s: complex[]): complex[] {
+    let y = fft(s.map(z => z.conjugate));
+    return y.map(z => z.conjugate.times_real(1.0 / s.length));
+}
+
