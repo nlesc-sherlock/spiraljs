e62145f8d123be5a947356226641ecde3ab70ab7
fdiblen <f.diblen@esciencecenter.nl>
2017-02-15 13:03:59 +0100

    Fixes code coverage issues. Karma and webpack settings are stored in config folder. Updated packages to the latest versions. Typedoc only works with version 0.5.1.

---
+++ config/webpack.prod.js
@@ -0,0 +1,31 @@
+﻿var webpack = require('webpack');
+var webpackMerge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var commonConfig = require('./webpack.common.js');
+var helpers = require('./helpers');
+
+module.exports = webpackMerge(commonConfig, {
+    devtool: 'hidden-source-map',
+
+    output: {
+        path: helpers.root('static'),
+        publicPath: 'static/',
+        filename: 'js/[name].[hash].js',
+        chunkFilename: 'js/[id].[hash].chunk.js'
+    },
+
+    htmlLoader: {
+        minimize: false
+    },
+
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            mangle: {
+                keep_fnames: true
+            }
+        }),
+        new ExtractTextPlugin('css/[name].[hash].css')
+    ]
+});
---
+++ config/webpack.test.js
@@ -0,0 +1,45 @@
+﻿var helpers = require('./helpers');
+
+module.exports = {
+    devtool: 'inline-source-map',
+
+    resolve: {
+        extensions: ['', '.ts', '.js']
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: [
+                    'awesome-typescript-loader?sourceMap=false,inlineSourceMap=true'
+                ]
+            },
+            {
+                test: /\.html$/,
+                loader: 'html'
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                loader: 'null'
+            },
+            {
+                test: /\.less$/,
+                exclude: helpers.root('app'),
+                loader: 'null'
+            },
+            {
+                test: /\.css$/,
+                include: helpers.root('app'),
+                loader: 'raw'
+            }
+        ],
+        postLoaders: [
+            {
+                test: /\.ts$/,
+                exclude: /(node_modules|app\\spec)/,
+                loader: 'istanbul-instrumenter'
+            }
+        ]
+    }
+};
